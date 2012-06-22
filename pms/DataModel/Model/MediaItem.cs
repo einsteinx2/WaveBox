4bae2d2fbe57cf1fe824c9d39ade56cc1e8992d1
Justin <jhill.d@gmail.com>
2012-06-22 12:27:10 -0400

    Implemented MediaItem data model class

--- pms/DataModel/Model/MediaItem.cs
+++ pms/DataModel/Model/MediaItem.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Data.SqlServerCe;
+using pms.DataModel.Model;
+using pms.DataModel.Singletons;
 
 namespace pms.DataModel.Model
 {
@@ -164,9 +167,47 @@ namespace pms.DataModel.Model
 		{
 		}
 
-		public static bool fileNeedsUpdating(FileStream file)
+		public static bool fileNeedsUpdating(FileInfo file)
 		{
-			return true;
+			int folderId = new Folder(file.Directory.ToString()).FolderId;
+			string fileName = file.Name;
+			long lastModified = Convert.ToInt64(file.LastWriteTime);
+			bool needsUpdating = true;
+
+			SqlCeConnection conn = null;
+			SqlCeDataReader reader = null;
+
+			try
+			{
+				conn = Database.getDbConnection();
+
+				string query = string.Format("SELECT COUNT(*) AS count FROM song WHERE song_folder_id = {0} AND song_file_name = {1} ANd song_last_modified = {2}",
+					folderId, fileName, lastModified);
+
+				var q = new SqlCeCommand(query);
+				q.Connection = conn;
+				q.Prepare();
+				reader = q.ExecuteReader();
+
+				if (reader.Read())
+				{
+					if (reader.GetInt32(0) >= 1)
+					{
+						needsUpdating = false;
+					}
+				}
+			}
+
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			finally
+			{
+				Database.close(conn, reader);
+			}
+			return needsUpdating;
 		}
 	}
 }
