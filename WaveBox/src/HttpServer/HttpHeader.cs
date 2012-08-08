6908a8c381cfaf88d0fe7d353ac3462a6260c1f0
Justin <jhill.d@gmail.com>
2012-08-08 11:15:08 -0400

    HttpProcessor modifications
    - Modified HttpProcessor to send appropriate MIME types for different
    media formats
    
    - Removed the redundant SendBytes method from HttpProcessor

--- WaveBox/src/HttpServer/HttpHeader.cs
+++ WaveBox/src/HttpServer/HttpHeader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace WaveBox.Http
 {
@@ -17,6 +18,44 @@ namespace WaveBox.Http
 			NOTFOUND = 1
 		}
 
+        public enum HttpContentType
+        {
+            AUDIOMP3 = 0,
+            AUDIOMP4 = 1,
+            AUDIOOGG = 2,
+            AUDIOWEBM = 3,
+            AUDIOWAV = 4,
+            VIDEOWEBM = 5,
+            VIDEOMP4 = 6,
+            VIDEOOGG = 7,
+            UNKNOWN = 8
+        }
+
+        private string[] HttpContentTypeStrings = { "audio/mpeg", "audio/mp4", "audio/ogg", "audio/webm", "audio/wav", "video/webm", "video/mp4", "video/ogg", "octet-stream" };
+
+        public static HttpContentType ContentTypeForExtension(string extension)
+        {
+            string ext = extension.ToLower();
+
+            if (ext == ".mp3")
+                return HttpContentType.AUDIOMP3;
+            else if (ext == ".m4a")
+                return HttpContentType.AUDIOMP4;
+            else if (ext == ".ogg" || ext == "oga")
+                return HttpContentType.AUDIOOGG;
+            else if (ext == ".webma")
+                return HttpContentType.AUDIOWEBM;
+            else if (ext == ".wav")
+                return HttpContentType.AUDIOWAV;
+            else if (ext == ".mp4" || ext == ".m4v")
+                return HttpContentType.VIDEOMP4;
+            else if (ext == ".ogv") 
+                return HttpContentType.VIDEOOGG;
+            else if (ext == ".webm" || ext == ".webmv")
+                return HttpContentType.VIDEOWEBM;
+            else return HttpContentType.UNKNOWN;
+        }
+
 		public HttpHeader()
 		{
 			StatusCode = HttpStatusCode.OK;
@@ -24,23 +63,23 @@ namespace WaveBox.Http
 			ContentLength = 0;
 		}
 
-		public HttpHeader(HttpStatusCode s, string cType, long cLen)
+		public HttpHeader(HttpStatusCode s, HttpContentType cType, long cLen)
 		{
 			StatusCode = s;
-			ContentType = cType;
+			ContentType = HttpContentTypeStrings[(int)cType];
 			ContentLength = cLen;
 		}
 
-		public void WriteHeader(HttpProcessor hp)
+		public void WriteHeader(StreamWriter outputStream)
 		{
 			// write the headers
-			hp.OutputStream.Write(HeaderStringWithResponseCode(StatusCode) + "\r\n");
-			hp.OutputStream.Write("Content-Length: " + ContentLength + "\r\n");
+			outputStream.Write(HeaderStringWithResponseCode(StatusCode) + "\r\n");
+			outputStream.Write("Content-Length: " + ContentLength + "\r\n");
 			if(ContentType != "")
-				hp.OutputStream.Write("Content-Type: " + ContentType + "\r\n");
+				outputStream.Write("Content-Type: " + ContentType + "\r\n");
 
 			// write the last newline signifying the end of the headers
-			hp.OutputStream.Write("\r\n");
+			outputStream.Write("\r\n");
 		}
 
 		public string HeaderStringWithResponseCode(HttpStatusCode c)
--- WaveBox/src/HttpServer/HttpProcessor.cs
+++ WaveBox/src/HttpServer/HttpProcessor.cs
@@ -262,8 +262,10 @@ namespace WaveBox.Http
 			FileInfo fsinfo = new FileInfo(fs.Name);
 
 			// Write the headers to output stream
-			long contentLength = length - startOffset;
-			WriteFileHeader(contentLength);
+            long contentLength = length - startOffset;
+            var header = new HttpHeader(HttpHeader.HttpStatusCode.OK, HttpHeader.ContentTypeForExtension(fsinfo.Extension), contentLength);
+            header.WriteHeader(OutputStream);
+            Console.WriteLine("[HTTPSERVER] File header, contentLength: {0}, contentType: {1}", contentLength, header.ContentType);
 
 			// Read/Write in 8 KB chunks
 			const int chunkSize = 8192;
@@ -321,81 +323,6 @@ namespace WaveBox.Http
 							Console.WriteLine("[SENDFILE] " + fsinfo.Name + ": Done.");
 							break;
 						}
-					}
-				}
-				catch (IOException e)
-				{
-					if (e.InnerException.GetType() == typeof(System.Net.Sockets.SocketException))
-					{
-						var se = (System.Net.Sockets.SocketException)e.InnerException;
-						if (se.SocketErrorCode == System.Net.Sockets.SocketError.ConnectionReset)
-						{
-							Console.WriteLine("[SENDFILE(2)] " + "Connection was forcibly closed by the remote host");
-						}
-					}
-
-					// Break the loop on error
-					break;
-				}
-			}
-			sw.Stop();
-			//_sh.writeFailure
-		}
-
-		public void sendBytes(byte[] bytes, int startOffset, long length)
-		{
-			if ((object)bytes == null || length == 0 || startOffset >= length)
-				return;
-
-			// Write the headers to output stream
-			long contentLength = length - startOffset;
-			HttpHeader h = new HttpHeader(HttpHeader.HttpStatusCode.OK, "", contentLength);
-			h.WriteHeader(this);
-
-			// Read/Write in 8 KB chunks
-			const int chunkSize = 8192;
-
-			// Initialize everything
-			int bytesRead = 0;
-			long bytesWritten = 0;
-			Stream stream = OutputStream.BaseStream;
-			int sinceLastReport = 0;
-			Stopwatch sw = new Stopwatch();
-
-			// Seek to the start offset
-			bytesWritten = startOffset;
-
-			sw.Start();
-			while(true)
-			{
-				try
-				{
-					// Either read the chunk size, or whatever's left
-					bytesRead = (int)(bytesWritten <= contentLength - chunkSize ? chunkSize : contentLength - bytesWritten);
-
-					// Send the bytes out to the client
-					// TODO: need to handle files larger than max int
-					stream.Write(bytes, (int)bytesWritten, bytesRead);
-					bytesWritten += bytesRead;
-
-					// Log the progress (only for testing)
-					if (sw.ElapsedMilliseconds > 1000)
-					{
-						Console.WriteLine("[SENDBYTES]: [ {0} / {1} | {2:F1}% | {3:F1} Mbps ]", bytesWritten, contentLength+startOffset, (Convert.ToDouble(bytesWritten) / Convert.ToDouble(contentLength+startOffset)) * 100, (((double)(sinceLastReport * 8) / 1024) / 1024) / (double)(sw.ElapsedMilliseconds / 1000));
-						sinceLastReport = 0;
-						sw.Restart();
-					}
-					else
-					{
-						sinceLastReport += bytesRead;
-					}
-
-					// See if we're done
-					if (bytesWritten >= length)
-					{
-						// We've written all the bytes, so break
-						Console.WriteLine("[SENDFILE]: Done.");
-						break;
 					}
 				}
 				catch (IOException e)
