aa710761f58034a2d3bd5267acbcdc3f11939eff
Matt Layher <mdlayher@gmail.com>
2013-05-13 23:45:37 -0400

    Added caching to extended status metrics

--- WaveBox/src/ApiHandler/Handlers/StatusApiHandler.cs
+++ WaveBox/src/ApiHandler/Handlers/StatusApiHandler.cs
@@ -15,12 +15,23 @@ using System.Runtime.Serialization.Formatters;
 
 namespace WaveBox.ApiHandler.Handlers
 {
+	class StatusApiCache
+	{
+		public IDictionary<string, object> Cache = new Dictionary<string, object>();
+
+		public long? LastQueryId { get; set; }
+	}
+
 	class StatusApiHandler : IApiHandler
 	{
 		private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
 		private IHttpProcessor Processor { get; set; }
 		private UriWrapper Uri { get; set; }
+
+		// Status API cache
+		private static StatusApiCache statusCache = new StatusApiCache();
+		public static StatusApiCache StatusCache { get { return statusCache; } }
 
 		/// <summary>
 		/// Constructor for StatusApiHandler class
@@ -86,22 +97,43 @@ namespace WaveBox.ApiHandler.Handlers
 				{
 					if (Uri.Parameters["extended"].IsTrue())
 					{
-						// Get count of artists
-						status["artistCount"] = Artist.CountArtists();
-						// Get count of albums
-						status["albumCount"] = Album.CountAlbums();
-						// Get count of songs
-						status["songCount"] = Song.CountSongs();
-						// Get count of videos
-						status["videoCount"] = Video.CountVideos();
-						// Get total file size of songs (bytes)
-						status["songFileSize"] = Song.TotalSongSize();
-						// Get total file size of videos (bytes)
-						status["videoFileSize"] = Video.TotalVideoSize();
-						// Get total song duration
-						status["songDuration"] = Song.TotalSongDuration();
-						// Get total video duration
-						status["videoDuration"] = Video.TotalVideoDuration();
+						// Check if any destructive queries have been performed since the last cache
+						if ((statusCache.LastQueryId == null) || (Database.LastQueryLogId() > statusCache.LastQueryId))
+						{
+							// Initialize cache if needed
+							if (statusCache.LastQueryId == null)
+							{
+								statusCache.LastQueryId = Database.LastQueryLogId();
+							}
+
+							logger.Info("Gathering extended status metrics from database");
+
+							// Get count of artists
+							statusCache.Cache["artistCount"] = Artist.CountArtists();
+							// Get count of albums
+							statusCache.Cache["albumCount"] = Album.CountAlbums();
+							// Get count of songs
+							statusCache.Cache["songCount"] = Song.CountSongs();
+							// Get count of videos
+							statusCache.Cache["videoCount"] = Video.CountVideos();
+							// Get total file size of songs (bytes)
+							statusCache.Cache["songFileSize"] = Song.TotalSongSize();
+							// Get total file size of videos (bytes)
+							statusCache.Cache["videoFileSize"] = Video.TotalVideoSize();
+							// Get total song duration
+							statusCache.Cache["songDuration"] = Song.TotalSongDuration();
+							// Get total video duration
+							statusCache.Cache["videoDuration"] = Video.TotalVideoDuration();
+
+							logger.Info("Metric gathering complete, cached results!");
+						}
+						else
+						{
+							logger.Info("Extended status metrics already cached!");
+						}
+
+						// Append cached status dictionary to status
+						status = status.Concat(statusCache.Cache).ToDictionary(x => x.Key, x => x.Value);
 					}
 				}
 
