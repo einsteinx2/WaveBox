bf2f9920ff602820e017389ff241436cc537850a
Matt Layher <mdlayher@gmail.com>
2013-05-20 15:58:04 -0400

    Add MPD cache and operation stubs, to ease future merges

---
+++ WaveBox.Server/src/TcpServer/MpdServer/MpdCache.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WaveBox.Model;
+using WaveBox.Static;
+
+namespace WaveBox.TcpServer.Mpd
+{
+	public class MpdCache
+	{
+		//private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+	}
+}
