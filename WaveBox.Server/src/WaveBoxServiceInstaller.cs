85550a501c4e8a2526cc1e35d821308b22d9f587
Benjamin Baron <ben@einsteinx2.com>
2013-07-17 19:24:28 +0200

    - Added some new mkbundle and nomkbundle scripts - Fixed Windows service install

---
+++ WaveBox.Server/src/WaveBoxServiceInstaller.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ServiceProcess;
+using System.Configuration.Install;
+using System.ComponentModel;
+
+namespace WaveBox
+{
+	[RunInstaller(true)]
+	public partial class WaveBoxServiceInstaller : Installer
+	{
+		private ServiceInstaller servInstaller;
+		private ServiceProcessInstaller serviceProcessInstaller;
+
+		public WaveBoxServiceInstaller()
+		{
+			InitializeComponent();
+
+			this.servInstaller = new ServiceInstaller();
+			this.serviceProcessInstaller = new ServiceProcessInstaller();
+
+			// Service Installer
+			this.servInstaller.DisplayName = "WaveBox Media Server";
+			this.servInstaller.ServiceName = "WaveBox";
+			this.servInstaller.StartType = ServiceStartMode.Automatic;
+			this.servInstaller.Description = "The WaveBox media server service, this runs in the background to allow clients to communicate with this computer.";
+
+			// Process Installer 
+			this.serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+			this.serviceProcessInstaller.Password = null;
+			this.serviceProcessInstaller.Username = null;
+
+			// Project Installer
+			this.Installers.AddRange(new System.Configuration.Install.Installer[] {this.servInstaller,this.serviceProcessInstaller});
+		}
+	}
+}
+
