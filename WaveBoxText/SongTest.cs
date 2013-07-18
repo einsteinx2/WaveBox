792f97aa7c5001cb0ceff463c00e8d33b97a3380
Benjamin Baron <ben@einsteinx2.com>
2013-07-19 01:21:22 +0200

    - Created new file watching solution for OS X based on the FSEvents API. This includes an Xcode project to build a dylib, and then some C# code to interface with it. This fixes the OS X bug where we immediately run out of open file handles with medium to large collections.

---
+++ WaveBoxText/SongTest.cs
@@ -0,0 +1,25 @@
+using System;
+using NUnit.Framework;
+using WaveBox.ApiHandler.Handlers;
+
+namespace WaveBoxText
+{
+	[TestFixture]
+	public class SongTest
+	{
+		[Test]
+		public void MakeSong()
+		{
+
+
+			//SongsApiHandler handler = new SongsApiHandler();
+			//SongResponse result = handler.Process();
+
+			//Assert.Equals(new List<Song>(), result.Songs);
+
+
+			
+		}
+	}
+}
+
