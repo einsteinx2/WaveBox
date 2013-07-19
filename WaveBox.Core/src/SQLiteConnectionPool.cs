60d1f50791d437cccecf7265ab37a96a9c1ec437
Benjamin Baron <ben@einsteinx2.com>
2013-07-19 17:12:25 +0200

    - Moved SQLiteConnectionPool class into WaveBox.Core assembly

---
+++ WaveBox.Core/src/SQLiteConnectionPool.cs
@@ -0,0 +1,72 @@
+using System;
+using Cirrious.MvvmCross.Plugins.Sqlite;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WaveBox
+{
+	public class SQLiteConnectionPool
+	{
+		private string databasePath = "";
+		private int maxConnections = 0;
+		private int usedConnections = 0;
+		private object connectionPoolLock = new object();
+		private Stack<ISQLiteConnection> availableConnections = new Stack<ISQLiteConnection>();
+
+		public SQLiteConnectionPool(int max, string path)
+		{
+			maxConnections = max;
+			databasePath = path;
+		}
+
+		public ISQLiteConnection GetSqliteConnection()
+		{
+			lock (connectionPoolLock)
+			{
+				ISQLiteConnection conn = null;
+				if (availableConnections.Count > 0)
+				{
+					// Grab an existing connection
+					conn = availableConnections.Pop();
+				}
+				else if (usedConnections < maxConnections)
+				{
+					// There are no available connections, and we have room for more open connections, so make a new one
+					conn = new SQLite.SQLiteConnection(databasePath);
+					conn.Execute("PRAGMA synchronous = OFF");
+					// Five second busy timeout
+					conn.BusyTimeout = new TimeSpan(0, 0, 5); 
+				}
+
+				if (!ReferenceEquals(conn, null))
+				{
+					// We got a connection, so increment the counter
+					usedConnections++;
+					return conn;
+				}
+			}
+
+			// If no connection available, sleep for 50ms and try again
+			Thread.Sleep(50);
+
+			// Recurse to try to get another connection
+			return GetSqliteConnection();
+		}
+
+		public void CloseSqliteConnection(ISQLiteConnection conn)
+		{
+			if (ReferenceEquals(conn, null))
+			{
+				return;
+			}
+
+			lock (connectionPoolLock)
+			{
+				// Make the connection available and decrement the counter
+				availableConnections.Push(conn);
+				usedConnections--;
+			}
+		}
+	}
+}
+
--- WaveBox.Server/src/Static/Database.cs
+++ WaveBox.Server/src/Static/Database.cs
@@ -27,13 +27,13 @@ namespace WaveBox.Static
 
 		// Sqlite connection pool
 		private static readonly int MAX_CONNECTIONS = 100;
-		private SqliteConnectionPool mainPool;
-		private SqliteConnectionPool logPool;
+		private SQLiteConnectionPool mainPool;
+		private SQLiteConnectionPool logPool;
 
 		public Database()
 		{
-			mainPool = new SqliteConnectionPool(MAX_CONNECTIONS, DatabasePath());
-			logPool = new SqliteConnectionPool(MAX_CONNECTIONS, QuerylogPath());
+			mainPool = new SQLiteConnectionPool(MAX_CONNECTIONS, DatabasePath());
+			logPool = new SQLiteConnectionPool(MAX_CONNECTIONS, QuerylogPath());
 		}
 
 		public void DatabaseSetup()
@@ -132,70 +132,6 @@ namespace WaveBox.Static
 			}
 
 			return -1;
-		}
-
-		private class SqliteConnectionPool
-		{
-			private string databasePath = "";
-			private int maxConnections = 0;
-			private int usedConnections = 0;
-			private object connectionPoolLock = new object();
-			private Stack<ISQLiteConnection> availableConnections = new Stack<ISQLiteConnection>();
-
-			public SqliteConnectionPool(int max, string path)
-			{
-				maxConnections = max;
-				databasePath = path;
-			}
-
-			public ISQLiteConnection GetSqliteConnection()
-			{
-				lock (connectionPoolLock)
-				{
-					ISQLiteConnection conn = null;
-					if (availableConnections.Count > 0)
-					{
-						// Grab an existing connection
-						conn = availableConnections.Pop();
-					}
-					else if (usedConnections < maxConnections)
-					{
-						// There are no available connections, and we have room for more open connections, so make a new one
-						conn = new SQLite.SQLiteConnection(databasePath);
-						conn.Execute("PRAGMA synchronous = OFF");
-						// Five second busy timeout
-						conn.BusyTimeout = new TimeSpan(0, 0, 5); 
-					}
-
-					if (!ReferenceEquals(conn, null))
-					{
-						// We got a connection, so increment the counter
-						usedConnections++;
-						return conn;
-					}
-				}
-
-				// If no connection available, sleep for 50ms and try again
-				Thread.Sleep(50);
-
-				// Recurse to try to get another connection
-				return GetSqliteConnection();
-			}
-
-			public void CloseSqliteConnection(ISQLiteConnection conn)
-			{
-				if (ReferenceEquals(conn, null))
-				{
-					return;
-				}
-
-				lock (connectionPoolLock)
-				{
-					// Make the connection available and decrement the counter
-					availableConnections.Push(conn);
-					usedConnections--;
-				}
-			}
 		}
 	}
 }
