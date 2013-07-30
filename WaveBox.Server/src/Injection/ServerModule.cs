5f85603cd6fd71c969cbf34fe1d0613e4d206f17
Matt Layher <mdlayher@gmail.com>
2013-07-30 16:59:53 -0400

    Add Injection folder

---
+++ WaveBox.Server/src/Injection/ServerModule.cs
@@ -0,0 +1,16 @@
+using System;
+using Ninject.Modules;
+using WaveBox.ApiHandler;
+
+namespace WaveBox.Server
+{
+	public class ServerModule : NinjectModule
+	{
+		public override void Load()
+		{
+			// API Authenticate and Factory
+			Bind<IApiAuthenticate>().To<ApiAuthenticate>().InSingletonScope();
+			Bind<IApiHandlerFactory>().To<ApiHandlerFactory>().InSingletonScope();
+		}
+	}
+}
