0709f2a4d816aefbd88f5fbfb6cf0e349ac2aff2
lontivero <lucasontivero@hotmail.com>
2014-08-11 02:56:56 -0300

    Update NatService.cs
    I have edited this file just to share some info and opinions about it related with Mono.Nat. I hope you find them useful.

--- WaveBox.Server/src/Service/Services/NatService.cs
+++ WaveBox.Server/src/Service/Services/NatService.cs
@@ -42,7 +42,18 @@ namespace WaveBox.Service.Services
 
 			// Hook into the events so you know when a router has been detected or has gone offline
 			NatUtility.DeviceFound += DeviceFound;
-			NatUtility.DeviceLost += DeviceLost;
+			
+			// Mono.Nat does never rise this event. The event is there however it is useless. 
+			// You could remove it with no risk.
+			//NatUtility.DeviceLost += DeviceLost;
+			
+                        // it is hard to say what one should do when an unhandled exception is raised
+                        // because there isn't anything one can do about it. Probably save a log or ignored it.
+                        // You assumption is that 'status = PortForwardingFailed' when this event is raised and 
+                        // that is wrong. 
+                        // This event is raised when something was wrong in the discovery process (a thread that
+                        // is continuely discovering) however it can fail after your portmapping was successfuly 
+                        // created. 
 			NatUtility.UnhandledException += UnhandledException;
 
 			// Start searching for upnp enabled routers
@@ -55,8 +66,17 @@ namespace WaveBox.Service.Services
 		{
 			if ((object)Device != null && Status == NatStatus.PortForwardedSuccessfully)
 			{
-				Device.DeletePortMap(new Mapping(Protocol.Tcp, Injection.Kernel.Get<IServerSettings>().Port, Injection.Kernel.Get<IServerSettings>().Port));
+				// As I said before, this is not okey. Just imagine this scenario:
+				// 1. Mono.Nat starts discovering and finds a router
+				// 2. You create a port mapping successfully
+				// 3. Given you never stopped the discovery process, it continues discovering
+				// 4. Two hours later it fails and raises an unhandled exception event
+				// 5. You handle the event and set Status = NatStatus.PortForwardingFailed - however there is nothing wrong with the created port mapping 
+				// 6. When invoked, this method will not delete the mapping!
 				Status = NatStatus.NotInitialized;
+				Device.DeletePortMap(new Mapping(Protocol.Tcp, Injection.Kernel.Get<IServerSettings>().Port, Injection.Kernel.Get<IServerSettings>().Port));
+
+				// DeletePortMapping can fail so, Status should be set before 
 			}
 
 			return true;
@@ -84,6 +104,8 @@ namespace WaveBox.Service.Services
 			}
 		}
 
+		// This is not funny, all developers in all the projects that I saw, spend time thinking 
+		// about what they should do when a device is lost however the event is a bluff.
 		private void DeviceLost(object sender, DeviceEventArgs args)
 		{
 			this.Status = NatStatus.PortForwardingFailed;
@@ -96,7 +118,7 @@ namespace WaveBox.Service.Services
 
 		private void UnhandledException(object sender, UnhandledExceptionEventArgs args)
 		{
-			this.Status = NatStatus.PortForwardingFailed;
+			this.Status = NatStatus.PortForwardingFailed; // you have a problem here
 
 			logger.Error("Unhandled exception: " + args);
 		}
